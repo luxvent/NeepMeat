a73c9123e4ea8a14edc1c0e31c32d3c59f86e966
MeatWheeze <ooo@oo.com>
2024-04-12 14:43:54 +0100

    Added asbestos ore generation and recipes

--- src/main/java/com/neep/neepmeat/world/NMFeatures.java
+++ src/main/java/com/neep/neepmeat/world/NMFeatures.java
@@ -2,6 +2,8 @@ package com.neep.neepmeat.world;
 
 import com.neep.neepmeat.NeepMeat;
 import com.neep.neepmeat.init.NMBlocks;
+import net.fabricmc.fabric.api.biome.v1.BiomeModifications;
+import net.fabricmc.fabric.api.biome.v1.BiomeSelectors;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricDynamicRegistryProvider;
 import net.minecraft.block.BlockState;
@@ -9,23 +11,32 @@ import net.minecraft.block.Blocks;
 import net.minecraft.registry.RegistryKey;
 import net.minecraft.registry.RegistryKeys;
 import net.minecraft.registry.RegistryWrapper;
+import net.minecraft.registry.tag.BlockTags;
+import net.minecraft.structure.rule.RuleTest;
+import net.minecraft.structure.rule.TagMatchRuleTest;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.collection.DataPool;
 import net.minecraft.util.math.intprovider.ConstantIntProvider;
 import net.minecraft.util.math.intprovider.UniformIntProvider;
-import net.minecraft.world.gen.feature.ConfiguredFeature;
-import net.minecraft.world.gen.feature.Feature;
-import net.minecraft.world.gen.feature.TreeFeatureConfig;
+import net.minecraft.world.gen.GenerationStep;
+import net.minecraft.world.gen.YOffset;
+import net.minecraft.world.gen.feature.*;
 import net.minecraft.world.gen.feature.size.TwoLayersFeatureSize;
 import net.minecraft.world.gen.foliage.RandomSpreadFoliagePlacer;
+import net.minecraft.world.gen.placementmodifier.*;
 import net.minecraft.world.gen.stateprovider.BlockStateProvider;
 import net.minecraft.world.gen.stateprovider.WeightedBlockStateProvider;
 import net.minecraft.world.gen.trunk.BendingTrunkPlacer;
 
+import java.util.List;
 import java.util.concurrent.CompletableFuture;
 
 public class NMFeatures extends FabricDynamicRegistryProvider
 {
+    public static final RegistryKey<ConfiguredFeature<?, ?>> ORE_ASBESTOS = RegistryKey.of(RegistryKeys.CONFIGURED_FEATURE, new Identifier(NeepMeat.NAMESPACE, "ore_asbestos"));
+    public static final RegistryKey<PlacedFeature> ORE_ASBESTOS_UPPER = RegistryKey.of(RegistryKeys.PLACED_FEATURE, new Identifier(NeepMeat.NAMESPACE, "ore_asbestos_upper"));
+    public static final RegistryKey<PlacedFeature> ORE_ASBESTOS_LOWER = RegistryKey.of(RegistryKeys.PLACED_FEATURE, new Identifier(NeepMeat.NAMESPACE, "ore_asbestos_lower"));
+
     public NMFeatures(FabricDataOutput output, CompletableFuture<RegistryWrapper.WrapperLookup> registriesFuture)
     {
         super(output, registriesFuture);
@@ -34,6 +45,24 @@ public class NMFeatures extends FabricDynamicRegistryProvider
     public static void init()
     {
 //        MeatLibDataGen.register(NMFeatures::new);
+
+        BiomeModifications.addFeature(BiomeSelectors.foundInOverworld(), GenerationStep.Feature.UNDERGROUND_ORES, ORE_ASBESTOS_UPPER);
+        BiomeModifications.addFeature(BiomeSelectors.foundInOverworld(), GenerationStep.Feature.UNDERGROUND_ORES, ORE_ASBESTOS_LOWER);
+    }
+
+    private static List<PlacementModifier> modifiers(PlacementModifier countModifier, PlacementModifier heightModifier)
+    {
+        return List.of(countModifier, SquarePlacementModifier.of(), heightModifier, BiomePlacementModifier.of());
+    }
+
+    private static List<PlacementModifier> modifiersWithCount(int count, PlacementModifier heightModifier)
+    {
+        return modifiers(CountPlacementModifier.of(count), heightModifier);
+    }
+
+    private static List<PlacementModifier> modifiersWithRarity(int chance, PlacementModifier heightModifier)
+    {
+        return modifiers(RarityFilterPlacementModifier.of(chance), heightModifier);
     }
 
     @Override
@@ -51,6 +80,16 @@ public class NMFeatures extends FabricDynamicRegistryProvider
                 new RandomSpreadFoliagePlacer(ConstantIntProvider.create(3), ConstantIntProvider.create(0), ConstantIntProvider.create(2), 50),
                 new TwoLayersFeatureSize(1, 0, 1)).dirtProvider(BlockStateProvider.of(Blocks.ROOTED_DIRT)).forceDirt().build();
         entries.add(RegistryKey.of(RegistryKeys.CONFIGURED_FEATURE, new Identifier(NeepMeat.NAMESPACE, "blood_bubble_tree")), new ConfiguredFeature<>(Feature.TREE, thing));
+
+        RuleTest ruleTest = new TagMatchRuleTest(BlockTags.BASE_STONE_OVERWORLD);
+        entries.add(ORE_ASBESTOS, new ConfiguredFeature<>(Feature.ORE, new OreFeatureConfig(ruleTest, NMBlocks.ASBESTOS.getDefaultState(), 64)));
+        placedFeature(ORE_ASBESTOS_UPPER, ORE_ASBESTOS, entries, modifiersWithRarity(6, HeightRangePlacementModifier.uniform(YOffset.fixed(64), YOffset.fixed(128))));
+        placedFeature(ORE_ASBESTOS_LOWER, ORE_ASBESTOS, entries, modifiersWithCount(1, HeightRangePlacementModifier.uniform(YOffset.fixed(0), YOffset.fixed(60))));
+    }
+
+    private static void placedFeature(RegistryKey<PlacedFeature> registryKey, RegistryKey<ConfiguredFeature<?, ?>> configured, Entries entries, List<PlacementModifier> modifiers)
+    {
+        entries.add(registryKey, new PlacedFeature(entries.ref(configured), modifiers));
     }
 
     @Override
